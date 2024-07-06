7fd01225d1a630a324f373b895b5d071f36e13dd
parker <129785640+parkerstafford@users.noreply.github.com>
2024-07-05 19:05:42 -0700

    Created tasks

--- src/main.c
+++ src/main.c
@@ -1,11 +1,31 @@
 #include "rtos.h"
+#include "task.h"
+#include "hal.h"
 
-void main(void) {
-    rtos_init();
-
-    rtos_start();
-
+void task1(void *param) {
     while (1) {
-                
+        // Toggle LED
+        GPIOA->ODR ^= GPIO_ODR_ODR5;
+        task_yield();
     }
+}
+
+void task2(void *param) {
+    while (1) {
+        GPIOA->ODR ^= GPIO_ODR_ODR6;
+        task_yield();
+    }
+}
+
+int main(void) {
+    hal_init();
+    hal_timer_init();
+
+    task_create(task1, NULL);
+    task_create(task2, NULL);
+
+    task_start_scheduler();
+
+    while (1);
+    return 0;
 }
